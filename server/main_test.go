63ae217b90dd9891b117eefc67c3e25c2e751ed8
maksym <maximligomina011@gmail.com>
2025-01-16 20:01:10 +0200

    HTTP methods constraints

---
+++ server/main_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestCorrectFileSave(t *testing.T) {
+    tFilename := "testingfile.txt"
+    tFile, err := os.Create(tFilename)
+    if err != nil {
+ 	t.Fatal("Couldn't create a file.")
+    }
+    defer tFile.Close()
+    defer os.Remove(tFilename) 
+
+    errMsg, statusCode := saveUploadedFile(tFilename, tFile)
+    if errMsg != "" && statusCode != -1 {
+ 	t.Fatal()
+    }
+    
+    deleteFile(tFilename)
+}
+
+func TestSameFileSave(t *testing.T) {
+    tFilename := "testingfile.txt"
+    tFile, err := os.Create(tFilename)
+    if err != nil {
+	t.Fatal("Couldn't create a file.")
+    }
+    defer tFile.Close()
+    defer os.Remove(tFilename)
+
+    errMsg, statusCode := saveUploadedFile(tFilename, tFile)
+    if errMsg != "" && statusCode != -1 {
+	t.Fatal()
+    }
+    
+    errMsg, statusCode = saveUploadedFile(tFilename, tFile)
+    if errMsg != "File with the same name already exist" && statusCode != http.StatusBadRequest {
+	t.Fatal()
+    }
+
+    deleteFile(tFilename)
+}
+
+func TestDeleteFile(t *testing.T) {
+    tFilename := "testingfile.txt"
+    tFile, err := os.Create(tFilename)
+    if err != nil {
+	t.Fatal("Couldn't create a file.")
+    }
+    defer tFile.Close()
+    defer os.Remove(tFilename)
+
+    saveUploadedFile(tFilename, tFile) // should be OK 
+    
+    errMsg, code := deleteFile(tFilename)
+    if errMsg != "" && code != -1 {
+	t.Fatal()
+    }
+}
+
+func TestDeleteNonExistentFile(t *testing.T) {
+    tFilename := "GSLkGSGSoiyygybaoioBYSOOEktjTkenGdkjOehwlWQLhwqltnLNglsdkfjGLhl"
+    errMsg, code := deleteFile(tFilename)
+    if errMsg != "File don't exist." && code != http.StatusNoContent {
+	t.Fatal()
+    }
+}
+
