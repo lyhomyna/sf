2072976dfafedfbff544328420db9b775c9d0fc9
lyhomyna <maximligomina011@gmail.com>
2025-01-18 13:39:57 +0200

    Created frontend folder; added tailwind; created button

---
+++ fe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Button from "./components/Button.js";
+
+function App() {
+  return (
+    <div className="App">
+	<Button text="Log in" />
+    </div>
+  );
+}
+
+export default App;
---
+++ fe/src/components/Button.js
@@ -0,0 +1,6 @@
+export default function Button({ text, className, ...props }) {
+    return (<button className={className+" rounded-lg text-neutral-400 bg-neutral-700 py-2 px-3 hover:text-neutral-300 duration-300"} {...props}>
+	{text}
+    </button>);
+}
+
