e2f6f4fb7853fd1bbf60e86f8de24505ff3f2e0d
maksym <maximligomina011@gmail.com>
2025-01-21 15:07:51 +0200

    First version of fe

--- fe/src/components/FileItem.jsx
+++ fe/src/components/FileItem.jsx
@@ -1,14 +1,19 @@
+import Button from "./Button.jsx";
+
 export default function FileItem({ fullFilename }) {
     const [filename, ext] = fullFilename.split(".")
-    return (<li className="flex flex-row gap-[0.3rem] items-center mt-3">
-	<button className="w-[15px] h-[2.1rem] bg-red-400" title="Delete" />
-	<div className="flex flex-row gap-x-2">
-	    <div className="border border-stone-300 text-slate-300 p-1">
-		.{ ext }
+    return (<li className="flex flex-row justify-between gap-2 items-center mt-3">
+	<div className="flex gap-[0.3rem]">
+	    <button className="w-[15px] h-[2.1rem] bg-red-400 hover:bg-red-700 duration-300" title="Delete" />
+	    <div className="flex flex-row gap-x-2">
+		<div className="border border-stone-300 text-slate-300 p-1">
+		    .{ ext }
+		</div>
+		<p className="text-xl text-slate-300" title={fullFilename}>
+		    { fullFilename }
+		</p>
 	    </div>
-	    <p className="text-xl text-slate-300" title={fullFilename}>
-		{ fullFilename }
-	    </p>
 	</div>
+	<Button text="Download"/>
     </li>);
 }
