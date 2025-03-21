f108b121fad9b88f74b1960d42caf6639808f421
lyhomyna <maximligomina011@gmail.com>
2025-03-22 00:47:02 +0200

    conn busy

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -9,34 +9,39 @@ export const fileServiceBaseUrl = "/api/files"
 
 export default function App() {
     const [isAuthenticated, setAuthenticated] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(true);
 
     const changeAuthStatus = () => {
 	setAuthenticated(current => !current);
     }
 
     useEffect(() => {
-	const checkAuth = async () => {
+	(async () => {
 	    const res = await fetch(`${authServiceBaseUrl}/check-auth`);
 	    
 	    if (res.status === 200) {
+		setIsLoading(false);
 		setAuthenticated(true);
 		return;
 	    }
+	    setIsLoading(false);
 	    setAuthenticated(false);
-	};
-	checkAuth();
+	})();
     }, []);
 
-    let page = <div className="flex justify-center items-center h-screen">
-	<RingLoader color="#58a6d8"/>
-    </div>;
-
-    if (isAuthenticated != undefined && !isAuthenticated) {
-	page = <div className="h-screen flex flex-col items-center justify-center">
-	   <LoginPage />
+    let page;
+    if (isLoading) {
+	page = <div className="flex justify-center items-center h-screen">
+	    <RingLoader color="#58a6d8"/>
 	</div>;
     } else {
-	page = <MainPage />;
+	if (isAuthenticated) {
+	    page = <MainPage />;
+	} else {
+	    page = <div className="h-screen flex flex-col items-center justify-center">
+	       <LoginPage />
+	    </div>;
+	}
     }
 
     return <AuthContext value={{changeAuthStatus: changeAuthStatus}}>
