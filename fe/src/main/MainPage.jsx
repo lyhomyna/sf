6533486c3badc206fc85656507dc8a5c8755afcc
lyhomyna <maximligomina011@gmail.com>
2025-04-15 01:05:56 +0300

    User email on the main page

--- fe/src/main/MainPage.jsx
+++ fe/src/main/MainPage.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import TopBar from "./components/TopBar.jsx";
 import FileList from "./components/FileList.jsx";
 import DragAndDrop from "./components/DragAndDrop.jsx";
 import { FilesContext } from "storage/SfContext.jsx";
+import { authServiceBaseUrl } from "config/constants.js";
 
 export default function MainPage() {
-    const email="supercoolemail@super.mail" 
     const imageURL="https://static.vecteezy.com/system/resources/previews/018/871/797/non_2x/happy-cat-transparent-background-png.png"
-
+    const [email, setEmail] = useState("supercoolemail@super.mail");
     const [filenames, setFilenames] = useState([]);
+
+    useEffect(() => {
+	const fetchUser = async () => {
+	    try {
+		const response = await fetch(`${authServiceBaseUrl}/get-user`);
+
+		if (response.status === 200) {
+		    const user = await response.json(); 
+		    setEmail(user.email);
+		} else if (response.status === 404) {
+		    const err = await response.json();
+		    console.log(err.message)
+		}
+	    } catch (err) {
+		console.log("Failed to fetch user", err)
+	    }
+	}
+
+	fetchUser();
+    }, [])
 
     const addFilenames = (filenames) => {
 	if (Array.isArray(filenames)) {
